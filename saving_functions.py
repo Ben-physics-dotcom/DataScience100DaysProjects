68d5b755e9916954600ac947afb3314507922029
Ben Fliegener <ben.j.fliegener1999@gmail.com>
2026-07-03 14:26:21 +0200

    updating saving_functions.py for classification results saving

--- saving_functions.py
+++ saving_functions.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 
 def saving_results(results: dict) -> None:
@@ -8,5 +9,17 @@ def saving_results(results: dict) -> None:
 
 def loading_results() -> dict:
     with open('../Results/regressions.pkl', 'rb') as f:
+        results = pickle.load(f)
+    return results
+
+
+def saving_classification_results(results: dict) -> None:
+    os.makedirs('../Results', exist_ok=True)
+    with open('../Results/classifications.pkl', 'wb') as f:
+        pickle.dump(results, f)
+
+
+def loading_classification_results() -> dict:
+    with open('../Results/classifications.pkl', 'rb') as f:
         results = pickle.load(f)
     return results
